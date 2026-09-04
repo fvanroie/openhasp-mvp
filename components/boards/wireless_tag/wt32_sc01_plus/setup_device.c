494ad2b2b0311e5faf5715ebf79cd5c3b330954b
fvanroie <fvanroie@users.noreply.github.com>
2026-09-04 15:27:54 +0000

    Fixing WT32-SC01 Plus config

--- components/boards/wireless_tag/wt32_sc01_plus/setup_device.c
+++ components/boards/wireless_tag/wt32_sc01_plus/setup_device.c
@@ -11,9 +11,20 @@ esp_err_t lcd_panel_factory_entry_t(
     const esp_lcd_panel_dev_config_t *panel_dev_config,
     esp_lcd_panel_handle_t *ret_panel)
 {
+    // Create a local copy of the configuration
+    esp_lcd_panel_dev_config_t local_panel_config = *panel_dev_config;
+
+    st7796_vendor_config_t st7796_vendor_cfg = {
+        .flags = {
+            .use_mipi_interface = false
+        }
+    };
+
+    local_panel_config.vendor_config = &st7796_vendor_cfg;
+
     return esp_lcd_new_panel_st7796(
         io,
-        panel_dev_config,
+        &local_panel_config,
         ret_panel
     );
 }
